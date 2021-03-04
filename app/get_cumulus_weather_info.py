6789af466d36617f20c9465c9a6aa8671f8ff5c8
crouchr <richard.crouch100@gmail.com>
2021-03-04 14:35:51 +0000

    bug fix

--- app/get_cumulus_weather_info.py
+++ app/get_cumulus_weather_info.py
@@ -1,4 +1,4 @@
-import call_rest_api
+import cumulus_comms
 from pprint import pprint
 
 
@@ -7,7 +7,7 @@ def get_key_weather_variables(cumulus_endpoint):
     Get some critical weather variables by querying the CumulusMX REST API
     """
 
-    status_code, response_dict = call_rest_api.call_rest_api(cumulus_endpoint, None)
+    status_code, response_dict = cumulus_comms.call_rest_api(cumulus_endpoint, None)
 
     # Aercus to CumulusMX serial connection down - all data now invalid
     if status_code == 200 and response_dict['DataStopped'] :
